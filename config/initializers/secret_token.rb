8da0080e9a8a13a54f1ea6ea72e4e2d9395bf74c
Robert Ohlzon <robert.ohlzon@d05.se>
2011-08-21 00:22:51 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whisper::Application.config.secret_token = '1923850c5621b8670ee902c933f2b991bcc71a96e413a55d101e3e6a94332bc580fee94a8520f4133a83316a544adca53843e54527a042c91ec35d290245d97c'
