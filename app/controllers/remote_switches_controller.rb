327e34d68f701ff3fb2430c4a0a0cc233e732763
Robert Ohlzon <robert.ohlzon@d05.se>
2011-08-24 00:56:49 +0200

    On/Off now works, as well as creation of new remote switches

--- app/controllers/remote_switches_controller.rb
+++ app/controllers/remote_switches_controller.rb
@@ -1,10 +1,16 @@
 class RemoteSwitchesController < ApplicationController
+  before_filter :find_remote_switch, :only => [:show, :on, :off]
+  private
+  def find_remote_switch
+    @remote_switch = RemoteSwitch.find(params[:id])
+  end
+  
+  public
   def index
     @remote_switches = RemoteSwitch.all
   end
 
   def show
-    @remote_switch = RemoteSwitch.find(params[:id])
   end
 
   def new
@@ -19,17 +25,17 @@ class RemoteSwitchesController < ApplicationController
   end
 
   def on
-    RemoteSwitch.new(params[:id]).on!
+    @remote_switch.on!
     time = Time.now
-    flash[:notice] = "Switched " + RemoteSwitch.find(params[:id]).label + " on at " + time.strftime("%H:%M:%S")
-    redirect_to :back
+    flash[:notice] = "Switched #{ @remote_switch.label } on at #{ Time.zone.now.strftime("%H:%M:%S") }"
+    redirect_to :back, :notice => "Switched #{ @remote_switch.label } on at #{ Time.zone.now.strftime("%H:%M:%S") }"
   end
   
   def off
-    RemoteSwitch.new(params[:id]).off!
+    @remote_switch.off!
     time = Time.now
-    flash[:notice] = "Switched " + RemoteSwitch.find(params[:id]).label + " off at " + time.strftime("%H:%M:%S")
-    redirect_to :back
+    flash[:notice] = "Switched #{ @remote_switch.label } off at #{ Time.zone.now.strftime("%H:%M:%S") }"
+    redirect_to :back, :notice => "Switched #{ @remote_switch.label } off at #{ Time.zone.now.strftime("%H:%M:%S") }"
   end
 
 end
--- app/models/remote_switch.rb
+++ app/models/remote_switch.rb
@@ -1,24 +1,17 @@
 class RemoteSwitch < ActiveRecord::Base
-  
-  def initialize(unit_id)
-    #Add verification of valid unit_id  
-    @unit_id = unit_id
-  end
-  
+
   def on!
-    # Quick and dirty output for verification...
-    time = Time.now
-    result = (`echo Switched on via a submethod in terminal at `)[0..-2] + " " + time.strftime("%H:%M:%S")
-    p result    
+    result = (`echo Switched on in terminal at `)[0..-2] + " " + Time.zone.now.strftime("%H:%M:%S")
+    p result
   end
-  
+
   def off!
-    # Quick and dirty output for verification...
-    time = Time.now
-    result = (`echo Switched #{@unit_id} off via a submethod in terminal at `)[0..-2] + " " + time.strftime("%H:%M:%S")
-    p result    
+    unit = RemoteSwitch.find(params[:id])
+    p unit
+    result = (`echo Switched off in terminal at `)[0..-2] + " " + Time.zone.now.strftime("%H:%M:%S")
+    p result
   end
-  
+
   private
-  
+
 end
