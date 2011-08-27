125153bdbb3c37b664e205b8a98595705c11fcf1
Robert Ohlzon <robert.ohlzon@d05.se>
2011-08-27 22:43:45 +0200

    Confirmation now shows name of handled switch

--- app/models/remote_switch.rb
+++ app/models/remote_switch.rb
@@ -1,14 +1,12 @@
 class RemoteSwitch < ActiveRecord::Base
 
   def on!
-    result = (`echo Switched on in terminal at `)[0..-2] + " " + Time.zone.now.strftime("%H:%M:%S")
+    result = (`echo Switched on #{label} in terminal at `)[0..-2] + " " + Time.zone.now.strftime("%H:%M:%S")
     p result
   end
 
   def off!
-    unit = RemoteSwitch.find(params[:id])
-    p unit
-    result = (`echo Switched off in terminal at `)[0..-2] + " " + Time.zone.now.strftime("%H:%M:%S")
+    result = (`echo Switched off #{label} in terminal at `)[0..-2] + " " + Time.zone.now.strftime("%H:%M:%S")
     p result
   end
 
