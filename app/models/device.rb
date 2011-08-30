bd8bc06ddfcdb897b2d319c858ec3f85b1b58589
Robert Ohlzon <robert.ohlzon@d05.se>
2011-08-31 01:02:12 +0200

    writeconfig now gives correct tellstick.conf output

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,19 @@
 class Device < ActiveRecord::Base
-    p '################ WRITE FILE ################'
   def self.writeconfig
+    configfile = File.open( "doc/tellstick.conf","w")
+    configfile.write 'deviceNode = "/dev/tellstick"' + "\n"
+    Device.all.each do |f|
+      configfile.write 'device {' + "\n"
+      configfile.write "  id = " + (f.id).to_s + "\n"
+      configfile.write "  name = " + f.name + "\n"
+      configfile.write "  model = " + f.model + "\n"
+      configfile.write "  parameters {" + "\n"
+      configfile.write "    house = " + (f.house).to_s + "\n"
+      configfile.write "    unit = " + (f.unit).to_s + "\n"
+      configfile.write "  }" + "\n"
+      configfile.write "}" + "\n"
+      configfile.write "\n"
+    end
+    configfile.close
   end
 end
