ac7b477679f6c388e812d5c079b137f201f69a0d
Robert Ohlzon <robert.ohlzon@d05.se>
2011-08-31 20:36:46 +0200

    Removed all references to remote_switches since they are replaced by devices

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Whisper::Application.routes.draw do
-  resources :remote_switches do
     get :on, :on => :member
     get :off, :on => :member
-  end
-  resources :devices do
     get :writeconfig, :on => :collection
   end
-  root :to => 'remote_switches#index'
 end
