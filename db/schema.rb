9c0238e18601059f62ccd312918e50254024deea
Robert Ohlzon <robert.ohlzon@d05.se>
2011-08-31 20:50:28 +0200

    Deleted remote_switches from schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,4 @@ ActiveRecord::Schema.define(:version => 20110830205410) do
     t.datetime "updated_at"
   end
 
-  create_table "remote_switches", :force => true do |t|
-    t.string   "label"
-    t.string   "location"
-    t.string   "kind"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
