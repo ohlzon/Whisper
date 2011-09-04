ba3560441a686c111ab21ba6e62037d76e41ff8f
Robert Ohlzon <robert.ohlzon@d05.se>
2011-09-04 02:20:54 +0200

    Removed experimental viewport Re-enabled javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,17 @@
-//$(document).ready(function(){
-//  $("a.on").click(function(event){  
-//    event.preventDefault();
-//    $.ajax({
-//      url: '/devices/' + $(this).attr('id') + '/on'
-//    });    
-//    $('p.' + $(this).attr('id') + 'state').replaceWith('<p class="' + $(this).attr('id') + 'state">on</p>');
-//  });
-//  
-//  $("a.off").click(function(event){  
-//    event.preventDefault();
-//    $.ajax({
-//      url: '/devices/' + $(this).attr('id') + '/off'
-//    });    
-//    $('p.' + $(this).attr('id') + 'state').replaceWith('<p class="' + $(this).attr('id') + 'state">off</p>');
-//  });
-//});
+$(document).ready(function(){
+  $("a.on").click(function(event){  
+    event.preventDefault();
+    $.ajax({
+      url: '/devices/' + $(this).attr('id') + '/on'
+    });    
+    $('p.' + $(this).attr('id') + 'state').replaceWith('<p class="' + $(this).attr('id') + 'state">on</p>');
+  });
+  
+  $("a.off").click(function(event){  
+    event.preventDefault();
+    $.ajax({
+      url: '/devices/' + $(this).attr('id') + '/off'
+    });    
+    $('p.' + $(this).attr('id') + 'state').replaceWith('<p class="' + $(this).attr('id') + 'state">off</p>');
+  });
+});
